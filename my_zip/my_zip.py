796bf732f354900172e5fe830ab833604360c511
Ali Shareef <aliishareef07@gmail.com>
2026-07-24 15:50:17 +0800

    puzzle 23 incompleate

--- my_zip/my_zip.py
+++ my_zip/my_zip.py
@@ -1,14 +1,16 @@
 def my_zip(input_list_a: list[any], input_list_b: list[any])->list[any]:
     newlist = []
-    if len(input_list_a) <= len(input_list_b):
-        for i in range(0,len(input_list_a),1):
+    smaller_array_length = len(input_list_a) if len(input_list_a) <= len(input_list_b) else len(input_list_b)     
+    for i in range(0,smaller_array_length,1):
             newlist.append((input_list_a[i], input_list_b[i]))
-    else: 
-        for i in range(0,len(input_list_b),1):
-            newlist.append((input_list_a[i], input_list_b[i]))        
+          
     return newlist
 
 
-print(my_zip([1,2,3,4],[5,6]))  
+def recursive_my_zip(input_list_a: list[any], input_list_b: list[any])->list[any]:
+    smaller_array = input_list_a if len(input_list_a) <= len(input_list_b) else input_list_b
+ 
+    
+print(my_zip([1,4, 5],[2, 4]))  
             
-            
+             
